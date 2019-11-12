2dc792c34e37477e33966aa7695dcb75df7f3352
Max Bendick <max@repostnetwork.com>
2019-11-12 15:17:39 -0800

    Export public API

--- projects/ngrx-data-pagination/src/public-api.ts
+++ projects/ngrx-data-pagination/src/public-api.ts
@@ -2,5 +2,11 @@
  * Public API Surface of ngrx-data-pagination
  */
 
-export * from './lib/ngrx-data-pagination.service';
 export * from './lib/ngrx-data-pagination.module';
+export * from './lib/ngrx-data-pagination.service';
+export {
+  NgrxPaginationContext,
+} from './lib/pagination/store-interfaces/ngrx/ngrx-pagination-context';
+export {
+  StorePaginationContext,
+} from './lib/pagination/store-interfaces/store-pagination-context';
