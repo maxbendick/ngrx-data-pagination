678fc68c77e381e252e62d052c436ef238e0923e
Max Bendick <maxbendick@gmail.com>
2019-11-30 21:12:10 -0800

    More and quicker example heroes

--- projects/demo-app/src/app/services/example-heroes.ts
+++ projects/demo-app/src/app/services/example-heroes.ts
@@ -1,13 +1,41 @@
 import { Hero } from '../models/hero';
 
-export const exampleHeroes: Hero[][] = [
-  [
-    { id: '1', name: 'a' },
-    { id: '2', name: 'b' },
-    { id: '3', name: 'c' },
-  ],
-  [
-    { id: '4', name: 'd' },
-    { id: '5', name: 'e' },
-  ],
-];
+export const heroPageSize = 10;
+
+// Will create 52 items, so 6th page will have 2 items
+function* heroGenerator() {
+  for (let i = 0; i < 26; i++) {
+    const char = String.fromCharCode(97 + i);
+    yield {
+      id: `${i}`,
+      name: `${char}${char}${char}`,
+    };
+  }
+  for (let i = 0; i < 26; i++) {
+    const char = String.fromCharCode(65 + i);
+    yield {
+      id: `${i + 26}`,
+      name: `${char}${char}${char}`,
+    };
+  }
+}
+
+const heroes = heroGenerator();
+
+export const exampleHeroes: Hero[][] = [];
+
+let complete = false;
+
+for (let page = 0; !complete; page++) {
+  exampleHeroes.push([]);
+  for (let indexInPage = 0; indexInPage < heroPageSize; indexInPage++) {
+    const { value, done } = heroes.next();
+    if (value) {
+      exampleHeroes[page].push(value);
+    }
+    if (done) {
+      complete = true;
+      break;
+    }
+  }
+}
--- projects/demo-app/src/app/services/hero-pages.service.ts
+++ projects/demo-app/src/app/services/hero-pages.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { Hero } from '../models/hero';
-import { exampleHeroes } from './example-heroes';
+import {
+  exampleHeroes,
+  heroPageSize as exampleHeroPageSize,
+} from './example-heroes';
+
+export const heroPageSize = exampleHeroPageSize;
 
 @Injectable({ providedIn: 'root' })
 export class HeroPagesService {
   getPage(pageNumber: number): Observable<Hero[]> {
-    return of(exampleHeroes[pageNumber]).pipe(delay(3000));
+    return of(exampleHeroes[pageNumber]).pipe(delay(1000));
   }
 }
